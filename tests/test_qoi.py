85c95ba078509bad4557c9977e4b4a1fcb18c21b
kodonnell <kodonnell@users.noreply.github.com>
2021-12-07 07:42:26 +1300

    more tests for different colorspaces and rgba etc.

--- tests/test_qoi.py
+++ tests/test_qoi.py
@@ -5,24 +5,41 @@ import pytest
 import qoi
 
 # Create an empty 3-channel image
-rgb = np.random.randint(low=0, high=255, size=(224, 244, 3)).astype(np.uint8)
+RGB = np.random.randint(low=0, high=255, size=(224, 244, 3)).astype(np.uint8)
+RGBA = np.random.randint(low=0, high=255, size=(224, 244, 4)).astype(np.uint8)
 
 
 @pytest.mark.parametrize("is_path", [True, False])
-def test_write_read(tmp_path, is_path):
+@pytest.mark.parametrize("is_rgba", [True, False])
+@pytest.mark.parametrize(
+    "colorspace", [qoi.QOIColorSpace.SRGB, qoi.QOIColorSpace.LINEAR, qoi.QOIColorSpace.SRGB_LINEAR_ALPHA, None]
+)
+def test_write_read(tmp_path: Path, is_path: bool, colorspace: qoi.QOIColorSpace, is_rgba: bool):
     tmp_path = str(tmp_path) + ".qoi"
+    img = RGBA if is_rgba else RGB
     if is_path:
         tmp_path = Path(tmp_path)
-    bytes_written = qoi.write(tmp_path, rgb, qoi.QOIColorSpace.SRGB)
+    if colorspace is None:
+        bytes_written = qoi.write(tmp_path, img)
+    else:
+        bytes_written = qoi.write(tmp_path, img, colorspace)
     assert bytes_written > 0
-    rgb_read = qoi.read(tmp_path)
-    assert np.array_equal(rgb, rgb_read)
+    img_read = qoi.read(tmp_path)
+    assert np.array_equal(img, img_read)
 
 
-def test_encode_decode():
-    bites = qoi.encode(rgb, qoi.QOIColorSpace.SRGB)
-    rgb_decoded = qoi.decode(bites)
-    assert np.array_equal(rgb, rgb_decoded)
+@pytest.mark.parametrize("is_rgba", [True, False])
+@pytest.mark.parametrize(
+    "colorspace", [qoi.QOIColorSpace.SRGB, qoi.QOIColorSpace.LINEAR, qoi.QOIColorSpace.SRGB_LINEAR_ALPHA, None]
+)
+def test_encode_decode(colorspace: qoi.QOIColorSpace, is_rgba: bool):
+    img = RGBA if is_rgba else RGB
+    if colorspace is None:
+        bites = qoi.encode(img)
+    else:
+        bites = qoi.encode(img, colorspace)
+    img_decoded = qoi.decode(bites)
+    assert np.array_equal(img, img_decoded)
 
 
 def test_version():
@@ -31,4 +48,4 @@ def test_version():
 
 @pytest.mark.xfail
 def test_writes_without_extension(tmp_path):
-    qoi.write(str(tmp_path), rgb, qoi.QOIColorSpace.SRGB)
+    qoi.write(str(tmp_path), RGB, qoi.QOIColorSpace.SRGB)
