4df79a63174a9faee159561d74d31a198dad0b27
kodonnell <kodonnell@users.noreply.github.com>
2024-04-17 08:57:49 +1200

    added test for issue 26

---
+++ tests/test_ro_issue26.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+from tempfile import NamedTemporaryFile
+
+import numpy as np
+import pytest
+import qoi
+from PIL import Image
+
+
+@pytest.mark.xfail()
+def test_fails_with_ro_array_issue26():
+    image_filepath = Path(__file__).parent.parent / "src" / "qoi" / "koi.png"
+    image = Image.open(image_filepath).convert("RGB")
+    image_qoi = np.asarray(image)  # NB: using np.array make it RW as it takes a copy
+    assert not image_qoi.flags["WRITEABLE"]
+    with NamedTemporaryFile() as f:
+        _ = qoi.write(f.name, image_qoi)
